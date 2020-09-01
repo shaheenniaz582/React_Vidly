f5fefb9634cc998369ece351f52fa88119d478ff
Shaheen <shaheenniaz582@gmail.com>
2020-09-01 12:14:52 +0100

    movie component is built showing data in form of bootstrap table

---
+++ src/components/movies.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { getMovies }  from '../services/fakeMovieService'; // getMovie is a named export in fakeMovieService
+
+class Movies extends Component {
+    state = {  
+        movies: getMovies() // movies property is set to function here just for sake of understanding
+    };
+    render() { 
+        return ( 
+            // inserting bootstrap table here to display movies in form of table
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.movies.map(movie =>
+                       <tr key={movie._id}>
+                           <td>{ movie.title }</td>
+                           <td>{ movie.genre.name }</td> 
+                           <td>{ movie.numberInStock }</td>
+                           <td>{ movie.dailyRentalRate }</td>
+                       </tr> )}
+                </tbody>
+            </table>
+
+         );
+    }
+}
+ 
+export default Movies;
