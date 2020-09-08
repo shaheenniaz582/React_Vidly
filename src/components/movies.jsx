722b53455e495bcbc782a539a2068f84de1005d2
Shaheen <shaheenniaz582@gmail.com>
2020-09-08 12:13:30 +0100

    reusable like component is added only working on UI

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import Like from './common/like';
 import { getMovies }  from '../services/fakeMovieService'; // getMovie is a named export in fakeMovieService
 
 class Movies extends Component {
     state = {  
         movies: getMovies() // movies property is set to function here just for sake of understanding
     };
+
+    handleLike = movie => {
+        //console.log('Like clicked', movie);
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index] };
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies});
+    }
+
     // event hNDLER FOR ONCLICK arrow function used so no constructor or bining required here
     handleDelete = (movie) => {
         const movies = this.state.movies.filter( m => m._id !== movie._id);
         this.setState({ movies });
     }
+
     render() { 
         // some conditional rendering
         const { length: count } = this.state.movies; // here using destructuring assigning length
@@ -27,16 +39,26 @@ class Movies extends Component {
                         <th>Genre</th>
                         <th>Stock</th>
                         <th>Rate</th>
+                        <th />
+                        <th />
                     </tr>
                 </thead>
                 <tbody>
                     {this.state.movies.map(movie => (
-                        <tr key={movie._id}>
+                    <tr key={movie._id}>
                         <td>{ movie.title }</td>
                         <td>{ movie.genre.name }</td> 
                         <td>{ movie.numberInStock }</td>
                         <td>{ movie.dailyRentalRate }</td>
-                        <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td> 
+                        <td>
+                            <Like liked={movie.liked} onClick={() => this.handleLike(movie)}/>
+                        </td>
+                        <td>
+                            <button onClick={() => this.handleDelete(movie)} 
+                            className="btn btn-danger btn-sm">
+                            Delete
+                            </button>
+                        </td>
                     </tr>
                     ))}
                 </tbody>
